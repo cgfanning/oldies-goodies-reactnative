0d7ea41ea201a9b96be490f67b6f587aed3b5ba5
cgfanning <cgfanning@me.com>
2020-04-23 10:59:15 -0600

    Navigation part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import LocationInfo from './LocationInfoComponent';
-import { View, Platform  } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { View, Platform } from 'react-native';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -21,16 +22,46 @@ const DirectoryNavigator = createStackNavigator(
             }
         }
     }
-);//end DirectoryNavigator
+);
+
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home }
+    },
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator },
+        Directory: { screen: DirectoryNavigator }
+    },
+    {
+        drawerBackgroundColor: '#CEC8FF'
+    }
+);
 
 class Main extends Component {
     render() {
         return (
-            <View style = {{ flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-                <DirectoryNavigator />
+            <View style={{
+                flex: 1,
+                paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
+            }}>
+                <MainNavigator />
             </View>
-        );//end return
-    }//end render
-}//end class Main
+        );
+    }
+}
 
 export default Main;
